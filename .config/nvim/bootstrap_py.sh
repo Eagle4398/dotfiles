51332cc734da8c6a6507ea3e5403ec672e1341ce
Eagle4398 <177400222+Eagle4398@users.noreply.github.com>
2025-08-07 21:56:31 +0200

    fix hm script + nvim_py_bootstrap script

--- .config/nvim/bootstrap_py.sh
+++ .config/nvim/bootstrap_py.sh
@@ -12,23 +12,29 @@ for tool in "${REQUIRED_TOOLS[@]}"; do
     fi
 done
 
-if [ -d ".venv" ]; then
+if [ -d "$HOME/.config/nvim/.venv" ]; then
     echo ".venv already exists - skipping setup"
     exit 0
 fi
 
-if [ -f "uv.lock" ]; then
+if [ -f "$HOME/config/nvim/uv.lock" ]; then
     echo "Found uv.lock - syncing environment..."
+    cd "$HOME/config/nvim/"
     uv sync
     exit 0
 fi
 
 if [ ! -f "pyproject.toml" ]; then
     echo "Initializing new Python project with uv..."
+    cd "$HOME/config/nvim/"
     uv init
+    echo "Adding dependencies and creating environment/lockfile..."
+    uv add neovim "git+https://github.com/arne314/typstar.git"
+    echo "Environment setup complete"
+    exit 0
 fi
 
-echo "Adding dependencies and creating environment/lockfile..."
-uv add neovim "git+https://github.com/arne314/typstar.git"
+echo "Some kind of error..."
+exit 1 
 
-echo "Environment setup complete"
+
